6b22796b3743f8ee0a5ddf81f92ffd410c9a3710
Mohammed AbuSadeh <125093982+Mo-abusadeh@users.noreply.github.com>
2024-11-25 22:32:12 +0000

    Git Checking

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,5 @@
-package org.example;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is a test");
+        System.out.println("Checking if the git works");
     }
 }
